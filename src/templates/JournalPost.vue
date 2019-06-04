3b5270c8cdf40a10be0c050b0ceab0a916e42f6b
perrofeo <38986663+perrofeo@users.noreply.github.com>
2019-06-03 19:21:40 -0600

    Update JournalPost.vue

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -33,7 +33,7 @@ query JournalPost ($path: String!) {
   post: journalPost (path: $path) {
     title
     author
-    date (format: "MM/DD/YYYY")
+    date (format: "MM-YYYY")
     timeToRead
     content
   }
